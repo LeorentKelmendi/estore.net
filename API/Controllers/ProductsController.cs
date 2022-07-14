27fc935175ade410763edd42d1f7b47c2eae217e
leokel <leo.kelmendi@bythjul.com>
2022-07-14 12:00:04 +0200

    formatting

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -14,20 +14,19 @@ namespace API.Controllers
         {
             this._context = context;
         }
- 
+
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-          return await _context.Products.ToListAsync();
+            return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
 
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+             return await _context.Products.FindAsync(id);
         }
-
 
     }
 }
